aa9d2ac2c3cf12d349a59f2e9cec4f8c33aea252
Eriks Vitolins <evitolins@gmail.com>
2015-02-15 03:16:09 -0800

    Hooked up oscillators to UI

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,21 +1,24 @@
 (function (document, Tone) { 'use strict';
 
     var elem = {
-        body : document.body,
         toggle : document.getElementById('btn_playToggle'),
         btnA : document.getElementById('btn_playA'),
         btnB : document.getElementById('btn_playB'),
         btnC : document.getElementById('btn_playC'),
-        btnD : document.getElementById('btn_playD')
+        btnD : document.getElementById('btn_playD'),
+        osc1Freq : document.getElementById('osc1_freq'),
+        osc2Freq : document.getElementById('osc2_freq'),
+        osc3Freq : document.getElementById('osc3_freq'),
+        osc4Freq : document.getElementById('osc4_freq')
     };
 
     // SYNTHS
     // ----------------------------------------------------------------------------
     //create one of Tone's built-in synthesizers
-    var synth1 = new Tone.MonoSynth();
-    var synth2 = new Tone.MonoSynth();
-    var synth3 = new Tone.MonoSynth();
-    var synth4 = new Tone.MonoSynth();
+    var synth1 = new Tone.AMSynth();
+    var synth2 = new Tone.AMSynth();
+    var synth3 = new Tone.AMSynth();
+    var synth4 = new Tone.AMSynth();
     synth1.toMaster();
     synth2.toMaster();
     synth3.toMaster();
@@ -23,20 +26,20 @@
 
     // OSCILLATORS
     // ----------------------------------------------------------------------------
-    var osc1 = new Tone.Oscillator(30, "triangle");
-    var osc2 = new Tone.Oscillator(32, "triangle");
-    var osc3 = new Tone.Oscillator(34, "triangle");
-    var osc4 = new Tone.Oscillator(36, "triangle");
+    var osc1 = new Tone.Oscillator(47, "sine");
+    var osc2 = new Tone.Oscillator(50, "sine");
+    var osc3 = new Tone.Oscillator(62, "sine");
+    var osc4 = new Tone.Oscillator(63, "sine");
     osc1.toMaster();
     osc2.toMaster();
     osc3.toMaster();
     osc4.toMaster();
 
-/*    
+    
     // ArpeggiatorEffect
     // ----------------------------------------------------------------------------
 
-    var synth = new Tone.MonoSynth();
+/*    var synth = new Tone.MonoSynth();
     var filter = new Tone.Filter();
 
     synth.connect(filter);
@@ -56,34 +59,18 @@
         var note = notes[position++];
         position = position % notes.length;
         synth.triggerAttackRelease(note, "8n", time);
-    }, "32n");
+    }, "8n");
 
-    //Tone.Transport.start();
+    Tone.Transport.start();*/
     
-*/
 
 
 
-    var playback = false;
+
     var synth1Active = false;
     var synth2Active = false;
     var synth3Active = false;
     var synth4Active = false;
-
-    var togglePlayback = function () {
-        playback = !playback;
-        if (playback) {
-            osc1.start();
-            osc2.start();
-            osc3.start();
-            osc4.start();
-        } else {
-            osc1.stop();
-            osc2.stop();
-            osc3.stop();
-            osc4.stop();
-        }
-    };
 
     var playA = function () {
         if (synth1Active) return;
@@ -125,14 +112,69 @@
     };
 
     /* DOM BINDING */
-    elem.toggle.addEventListener('click', function () {
-        togglePlayback();
-    });
     elem.btnA.addEventListener('mousedown', playA);
     elem.btnB.addEventListener('mousedown', playB);
     elem.btnC.addEventListener('mousedown', playC);
     elem.btnD.addEventListener('mousedown', playD);
     document.addEventListener('mouseup', stopPlay);
+
+
+    elem.osc1Freq.addEventListener('change', function () {
+       osc1.setFrequency(this.value);
+    });
+    elem.osc2Freq.addEventListener('change', function () {
+       osc2.setFrequency(this.value);
+    });
+    elem.osc3Freq.addEventListener('change', function () {
+       osc3.setFrequency(this.value);
+    });
+    elem.osc4Freq.addEventListener('change', function () {
+       osc4.setFrequency(this.value);
+    });
+
+
+    $("input[name=inlineRadioOptions1]").change(function () {
+        osc1.setType(this.value);
+    });
+    $("input[name=inlineRadioOptions2]").change(function () {
+        osc2.setType(this.value);
+    });
+    $("input[name=inlineRadioOptions3]").change(function () {
+        osc3.setType(this.value);
+    });
+    $("input[name=inlineRadioOptions4]").change(function () {
+        osc4.setType(this.value);
+    });
+
+    $("input[name=osc1_on]").change(function () {
+        if (this.value === "on") {
+            osc1.start();
+        } else {
+            osc1.stop();
+        }
+    });
+    $("input[name=osc2_on]").change(function () {
+        if (this.value === "on") {
+            osc2.start();
+        } else {
+            osc2.stop();
+        }
+    });
+    $("input[name=osc3_on]").change(function () {
+        if (this.value === "on") {
+            osc3.start();
+        } else {
+            osc3.stop();
+        }
+    });
+    $("input[name=osc4_on]").change(function () {
+        if (this.value === "on") {
+            osc4.start();
+        } else {
+            osc4.stop();
+        }
+    });
+
 
 
     Mousetrap.bind('q', playA, 'keypress');
